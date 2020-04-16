4a1222ff925cf7866d8b843bad6ddae44cbf770e
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-04-16 22:32:00 +0900

    コマンドライン引数の処理を書いている

---
+++ cusfeist.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type optvals struct {
+	src  string
+	dest string
+	pw   string
+}
+
+type subCommand interface {
+	name() string
+	run()
+}
+
+type encCommand struct {
+}
+
+func (c *encCommand) name() string {
+	return "enc"
+}
+
+func (c *encCommand) run() {
+	fmt.Println("run enc")
+}
+
+type decCommand struct {
+}
+
+func (c *decCommand) run() {
+	fmt.Println("run dec")
+}
+
+func (c *decCommand) name() string {
+	return "dec"
+}
+
+type helpCommand struct {
+}
+
+func (c *helpCommand) run() {
+	fmt.Println("run help")
+}
+
+func (c *helpCommand) name() string {
+	return "help"
+}
+
+func subCommands() []subCommand {
+	return []subCommand{
+		&encCommand{},
+		&decCommand{},
+		&helpCommand{},
+	}
+}
+
+func showCommands() {
+	fmt.Println(
+		"usage : cusfeist <subcommand> [options] [args]\n" +
+			"Type 'cusfeist help <subcommand>' for help on a specific subcommand.\n" +
+			"\nAvailable subcommands:")
+	for _, cmd := range subCommands() {
+		fmt.Println("  ", cmd.name())
+	}
+}
+
+func main() {
+	if 1 < len(os.Args) {
+		for _, cmd := range subCommands() {
+			if cmd.name() == os.Args[1] {
+				cmd.run()
+				return
+			}
+		}
+	}
+	showCommands()
+	return
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nabetani/cusfeist
+
+go 1.14
