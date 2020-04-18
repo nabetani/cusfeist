e821d66eafd530cb2beb5b140c03919d5c054950
Nabetani <takenori@nabetani.sakura.ne.jp>
2020-04-18 17:10:23 +0900

    内部状態で xoroshiro を使う、パスワードの扱いを間違っていたので修正

--- custCrypto.go
+++ custCrypto.go
@@ -3,23 +3,59 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/binary"
+	"fmt"
 	"math/bits"
 )
 
+type xoroshiro struct {
+	s [2]uint64
+}
+
+func newXoroshiro(seed []uint64) *xoroshiro {
+	read := func(v uint64) uint64 {
+		if v == 0 {
+			return 17071688163355054097
+		}
+		return v
+	}
+	s0 := read(seed[0])
+	s1 := read(seed[1])
+	return &xoroshiro{
+		s: [2]uint64{s0, s1},
+	}
+}
+
+func (x *xoroshiro) next() uint64 {
+	s0 := x.s[0]
+	s1 := x.s[1]
+
+	r := bits.RotateLeft64(s0+s1, 17) + s0
+	s1 ^= s0
+	x.s[0] = bits.RotateLeft64(s0, 49) ^ s1 ^ (s1 << 21) // a, b
+	x.s[1] = bits.RotateLeft64(s1, 28)                   // c
+	return r
+}
+
 type innerState struct {
-	v uint64
+	rng *xoroshiro
+	v   uint64
+	rot int
 }
 
 func (i *innerState) modify(x uint64) uint64 {
-	return x ^ i.v
+	return bits.RotateLeft64(x^i.v, i.rot)
 }
 
 func (i *innerState) progress() {
-	i.v += 11361801466989700884
+	i.v = i.rng.next()
+	i.rot = int(i.rng.next() & 63)
 }
 
-func newInnerState() *innerState {
-	return &innerState{}
+func newInnerState(seed []uint64) *innerState {
+	rng := newXoroshiro(seed)
+	r := &innerState{rng: rng}
+	r.progress()
+	return r
 }
 
 type custCrypto struct {
@@ -93,11 +129,12 @@ func newCustCrypto(pw string) *custCrypto {
 	makePw := func() []uint64 {
 		h := sha1.New()
 		h.Write([]byte("salt of custCrypto algorithm"))
-		pwBytes := []byte(pw)
+		pwBytes := []byte{}
 		for i := uint8(0); i < uint8(64); i++ {
-			h.Write([]byte(pwBytes))
+			h.Write([]byte(pw))
 			h.Write([]byte{i})
 			pwBytes = h.Sum(pwBytes)
+			fmt.Printf("pwBytes(%d): %v\n", i, pwBytes)
 		}
 		pw := []uint64{}
 		for {
@@ -109,9 +146,10 @@ func newCustCrypto(pw string) *custCrypto {
 		}
 		return pw
 	}
+	pw64 := makePw()
 	c := custCrypto{
-		pw: makePw(),
-		is: newInnerState(),
+		pw: pw64,
+		is: newInnerState(pw64),
 	}
 	return &c
 }
